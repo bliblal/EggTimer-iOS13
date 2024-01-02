23b5297e0f2a07d51a57e9306187d4a0ae94f9c9
Bilal <javaid.billal@gmail.com>
2024-01-02 11:42:21 +0500

    add progressbar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,19 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-//    var timerValues = ["Soft" : 300, "Medium" :420, "Hard": 720]
-    var timerValues = ["Soft" : 3, "Medium" :4, "Hard": 7]
+    var timerValues: [String:Float] = ["Soft" : 300, "Medium" :420, "Hard": 720]
+//    var timerValues: [String:Float] = ["Soft" : 3, "Medium" :4, "Hard": 7]
     var timer = Timer()
-    var secondsRemaining = 60
+    var secondsRemaining:Float = 60
+    var totalSeconds: Float = 0
     @IBOutlet weak var eggLabel: UILabel!
-
+    var progressValue: Float = 0
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBAction func eggPressed(_ sender: UIButton) {
         timer.invalidate()
         eggLabel.text = "How do you like your eggs?"
         secondsRemaining = timerValues[sender.currentTitle!]!
-        
+        totalSeconds = timerValues[sender.currentTitle!]!
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
+        eggLabel.text = sender.currentTitle
    
         
         
@@ -29,15 +32,24 @@ class ViewController: UIViewController {
     
     
     @objc func updateTimer(){
-        if (secondsRemaining>0){
+        if (secondsRemaining>=0){
             print("\(secondsRemaining) seconds")
+            let percent = getPercent(seconds: totalSeconds)
+            print(percent)
+            progressBar.progress = percent
             secondsRemaining -= 1
+            
         }
         else{
             timer.invalidate()
             eggLabel.text = "DONE"
         }
        
+    }
+    
+    func getPercent(seconds: Float) -> Float{
+        let percent: Float = 1-(secondsRemaining/seconds)
+        return Float(percent)
     }
     
    
